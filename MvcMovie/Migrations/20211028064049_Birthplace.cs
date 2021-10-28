b8744f59ab57dadd6e18c379a84aa5b6e900aa5c
Opilane TTHK <opilane@tthk.ee>
2021-10-28 09:57:25 +0300

    First Commit

---
+++ MvcMovie/Migrations/20211028064049_Birthplace.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcMovie.Migrations
+{
+    public partial class Birthplace : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Birthplace",
+                table: "Actors",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthplace",
+                table: "Actors");
+        }
+    }
+}
